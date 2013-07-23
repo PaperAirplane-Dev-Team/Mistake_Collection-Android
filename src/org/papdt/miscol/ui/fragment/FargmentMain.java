e7a635b197021b6d388874195e627b4d99608532
Harry Chen <373685963@qq.com>
2013-07-23 15:06:18 +0800

    以Git Submodule形式导入CardsUI库(有问题，详见FragmentMain的注释) 添加错题卡的布局和类（需要完善） 首次pull以后请运行git submodule update，并在Eclipse中导入libs/cardsui/CardsUILib项目
    Signed-off-by: Harry Chen <373685963@qq.com>

--- src/org/papdt/miscol/ui/fragment/FargmentMain.java
+++ src/org/papdt/miscol/ui/fragment/FargmentMain.java
@@ -1,7 +1,11 @@
 package org.papdt.miscol.ui.fragment;
 
 import org.papdt.miscol.R;
+import org.papdt.miscol.ui.MistakeCard;
 import org.papdt.miscol.utils.MyLogger;
+
+import com.fima.cardsui.objects.Card.OnCardSwiped;
+import com.fima.cardsui.views.CardUI;
 
 import android.app.Fragment;
 import android.os.Bundle;
@@ -10,9 +14,13 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class FargmentMain extends Fragment {
 
+	private CardUI mCardUI;
+	private LinearLayout mCardsLayout;
 	private final static String TAG = "FragmentMain";
 
 	public FargmentMain() {
@@ -21,20 +29,40 @@ public class FargmentMain extends Fragment {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-
 		super.onCreate(savedInstanceState);
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-
-		return super.onCreateView(inflater, container, savedInstanceState);
+		mCardsLayout = (LinearLayout) inflater.inflate(R.layout.fragment_main,
+				null);
+		mCardUI = (CardUI) mCardsLayout.findViewById(R.id.view_cardui);
+		mCardUI.setSwipeable(true);
+		MistakeCard card = new MistakeCard("Hello, World",
+				"Swipe to remove this card.");
+		card.setOnCardSwipedListener(new OnCardSwiped() {
+			@Override
+			public void onCardSwiped() {
+				Toast.makeText(getActivity(), "I'm being swiped!",
+						Toast.LENGTH_SHORT).show();
+				// XXX 不知道为什么没有用于移除Card的相关方法
+			}
+		});
+		mCardUI.addCard(card);
+		MyLogger.d(TAG, "Card已添加");
+		return card.getView(getActivity());
+		/* FIXME
+		 * 本来应该是return mCardUI的 谁知道报错Could not find class
+		 * 'com.fima.cardsui.objects.CardStack$3', referenced from method
+		 * com.fima.cardsui.objects.CardStack.getAnimationListener
+		 * 我一路追回去也不知道什么蛋疼的问题
+		 */
 	}
 
 	@Override
 	public void onPrepareOptionsMenu(Menu menu) {
-		//FIXME 不工作
+		// FIXME 不工作
 		MenuInflater inflater = getActivity().getMenuInflater();
 		inflater.inflate(R.menu.fragment_main, menu);
 		super.onPrepareOptionsMenu(menu);
