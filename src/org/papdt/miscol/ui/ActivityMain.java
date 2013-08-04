43ae6c12723499ccc28655178f398087739b4f0a
xavieryao <xavieryao@live.cn>
2013-08-05 00:05:25 +0800

    完成Welcome Fragment [x] 卡片正常工作 [x] 添加提示文字 [ ] 卡片的onSwipeListener不工作 [ ] 提示文字有问题……（“从最右”）

--- src/org/papdt/miscol/ui/ActivityMain.java
+++ src/org/papdt/miscol/ui/ActivityMain.java
@@ -9,14 +9,17 @@ import org.papdt.miscol.utils.MyLogger;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -36,7 +39,9 @@ public class ActivityMain extends Activity implements IDrawerNames {
 	private String[] mDrawerItemNames;
 	private FragmentManager fragmentManager;
 	private final static String TAG = "ActivityMain";
-
+	
+	private boolean hasDrawerOpened;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -52,10 +57,9 @@ public class ActivityMain extends Activity implements IDrawerNames {
 		MyLogger.d(TAG, TAG + "已完成初始化");
 	}
 
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		/*MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.activity_main, menu);*/
 		//在不同的Fragment中分开处理
 		return super.onCreateOptionsMenu(menu);
 	}
--- src/org/papdt/miscol/ui/fragment/FargmentMain.java
+++ src/org/papdt/miscol/ui/fragment/FargmentMain.java
@@ -7,6 +7,7 @@ import org.papdt.miscol.utils.MyLogger;
 import com.fima.cardsui.objects.Card.OnCardSwiped;
 import com.fima.cardsui.views.CardUI;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,13 +15,14 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 public class FargmentMain extends Fragment {
 
 	private CardUI mCardUI;
-	private LinearLayout mCardsLayout;
+	private FrameLayout mCardsLayout;
+	private Activity mActivity;
 	private final static String TAG = "FragmentMain";
 
 	public FargmentMain() {
@@ -35,13 +37,28 @@ public class FargmentMain extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		mCardsLayout = (LinearLayout) inflater.inflate(R.layout.fragment_main,
+		mActivity = getActivity();
+		mCardsLayout = (FrameLayout) inflater.inflate(R.layout.fragment_main,
 				null);
 		mCardUI = (CardUI) mCardsLayout.findViewById(R.id.view_cardui);
 		mCardUI.setSwipeable(true);
-		MistakeCard card = new MistakeCard("Hello, World",
+		MistakeCard cardWelcome = new MistakeCard(
+				mActivity.getString(R.string.welcome),
+				mActivity.getString(R.string.welcome_content));
+		cardWelcome.setOnCardSwipedListener(new OnCardSwiped() {
+			@Override
+			public void onCardSwiped() {
+				// XXX 不工作
+				Toast.makeText(getActivity(), "I'm being swiped!",
+						Toast.LENGTH_SHORT).show();
+				MyLogger.d(TAG, "being swiped");
+				// XXX 不知道为什么没有用于移除Card的相关方法
+			}
+		});
+		mCardUI.addCard(cardWelcome);
+		MistakeCard cardHello = new MistakeCard("Hello, World",
 				"Swipe to remove this card.");
-		card.setOnCardSwipedListener(new OnCardSwiped() {
+		cardHello.setOnCardSwipedListener(new OnCardSwiped() {
 			@Override
 			public void onCardSwiped() {
 				Toast.makeText(getActivity(), "I'm being swiped!",
@@ -49,15 +66,11 @@ public class FargmentMain extends Fragment {
 				// XXX 不知道为什么没有用于移除Card的相关方法
 			}
 		});
-		mCardUI.addCard(card);
+		mCardUI.addCard(cardHello);
+
+		mCardUI.refresh();
 		MyLogger.d(TAG, "Card已添加");
-		return card.getView(getActivity());
-		/* FIXME
-		 * 本来应该是return mCardUI的 谁知道报错Could not find class
-		 * 'com.fima.cardsui.objects.CardStack$3', referenced from method
-		 * com.fima.cardsui.objects.CardStack.getAnimationListener
-		 * 我一路追回去也不知道什么蛋疼的问题
-		 */
+		return mCardsLayout;
 	}
 
 	@Override
